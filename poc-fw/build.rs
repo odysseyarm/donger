6cc3f0606af6b1cd5d788341edc1e0671ad324d6
Matthew Tran <0e4ef622@gmail.com>
2025-05-05 21:03:12 -0500

    Add --print-memory-usage flag

--- poc-fw/build.rs
+++ poc-fw/build.rs
@@ -32,4 +32,5 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    println!("cargo:rustc-link-arg-bins=--print-memory-usage");
 }
